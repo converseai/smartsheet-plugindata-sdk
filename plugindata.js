0a1cec0ba4f977cce5cb8827af2b4a876b5fd70e
cassels <graeme.cassels@smartsheet.com>
2018-06-28 13:56:22 +0100

    Adding functions to plugindata

--- plugindata.js
+++ plugindata.js
@@ -8,4 +8,28 @@ module.exports = class PluginData extends GRPC {
       isSecure: false,
     });
   }
+
+  getPluginOAuth2Info(...args) {
+    this.client.getPluginOAuth2Info(...args);
+  }
+
+  storePluginOAuth2Info(...args) {
+    this.client.storePluginOAuth2Info(...args);
+  }
+
+  deletePluginOAuth2Info(...args) {
+    this.client.deletePluginOAuth2Info(...args);
+  }
+
+  getPluginData(...args) {
+    this.client.getPluginData(...args);
+  }
+
+  storePluginData(...args) {
+    this.client.storePluginData(...args);
+  }
+
+  deletePluginData(...args) {
+    this.client.deletePluginData(...args);
+  }
 };
