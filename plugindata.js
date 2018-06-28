3ece801d13782081cd0ae8e50181a5166d21c1da
cassels <graeme.cassels@smartsheet.com>
2018-06-28 14:24:53 +0100

    Correctly normalizing the correct plugindata but not the oauth2 info.

--- plugindata.js
+++ plugindata.js
@@ -21,29 +21,29 @@ module.exports = class PluginData extends GRPC {
   }
 
   getPluginOAuth2Info(data) {
-    return this.run('getPluginOAuth2Info', Buffer.from(JSON.stringify(data)))
-      .then(res => JSON.parse(res.data.toString('utf8')));
+    return this.run('getPluginOAuth2Info', data);
   }
 
   storePluginOAuth2Info(data) {
-    return this.run('storePluginOAuth2Info', Buffer.from(JSON.stringify(data)))
-      .then(res => JSON.parse(res.data.toString('utf8')));
+    return this.run('storePluginOAuth2Info', data);
   }
 
   deletePluginOAuth2Info(data) {
-    return this.run('deletePluginOAuth2Info', Buffer.from(JSON.stringify(data)))
-      .then(res => JSON.parse(res.data.toString('utf8')));
+    return this.run('deletePluginOAuth2Info', data);
   }
 
   getPluginData(data) {
-    return this.run('getPluginData', data);
+    return this.run('getPluginData', Buffer.from(JSON.stringify(data)))
+      .then(res => JSON.parse(res.data.toString('utf8')));
   }
 
   storePluginData(data) {
-    return this.run('storePluginData', data);
+    return this.run('storePluginData', Buffer.from(JSON.stringify(data)))
+      .then(res => JSON.parse(res.data.toString('utf8')));
   }
 
   deletePluginData(data) {
-    return this.run('deletePluginData', data);
+    return this.run('deletePluginData', Buffer.from(JSON.stringify(data)))
+      .then(res => JSON.parse(res.data.toString('utf8')));
   }
 };
