f95ee37ae23ce1142ae9126bfd26222840765a09
cassels <graeme.cassels@smartsheet.com>
2018-06-28 14:02:42 +0100

    ensuring the functions are returning the grpc response.

--- plugindata.js
+++ plugindata.js
@@ -10,26 +10,26 @@ module.exports = class PluginData extends GRPC {
   }
 
   getPluginOAuth2Info(...args) {
-    this.client.getPluginOAuth2Info(...args);
+    return this.client.getPluginOAuth2Info(...args);
   }
 
   storePluginOAuth2Info(...args) {
-    this.client.storePluginOAuth2Info(...args);
+    return this.client.storePluginOAuth2Info(...args);
   }
 
   deletePluginOAuth2Info(...args) {
-    this.client.deletePluginOAuth2Info(...args);
+    return this.client.deletePluginOAuth2Info(...args);
   }
 
   getPluginData(...args) {
-    this.client.getPluginData(...args);
+    return this.client.getPluginData(...args);
   }
 
   storePluginData(...args) {
-    this.client.storePluginData(...args);
+    return this.client.storePluginData(...args);
   }
 
   deletePluginData(...args) {
-    this.client.deletePluginData(...args);
+    return this.client.deletePluginData(...args);
   }
 };
